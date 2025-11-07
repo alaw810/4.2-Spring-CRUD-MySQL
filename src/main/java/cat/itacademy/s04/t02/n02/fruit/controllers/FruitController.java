9fc0e2e319f705d7a9a99bf62b6082374a08cbdb
Adrià Lorente <adria810@gmail.com>
2025-11-07 18:33:00 +0100

    refactor: change endpoints names

--- src/main/java/cat/itacademy/s04/t02/n02/fruit/controllers/FruitController.java
+++ src/main/java/cat/itacademy/s04/t02/n02/fruit/controllers/FruitController.java
@@ -3,7 +3,6 @@ package cat.itacademy.s04.t02.n02.fruit.controllers;
 import cat.itacademy.s04.t02.n02.fruit.dto.FruitRequestDTO;
 import cat.itacademy.s04.t02.n02.fruit.dto.FruitResponseDTO;
 import cat.itacademy.s04.t02.n02.fruit.services.FruitService;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,18 +21,18 @@ public class FruitController {
     }
 
     @PostMapping
-    public ResponseEntity<FruitResponseDTO> create(@Valid @RequestBody FruitRequestDTO request) {
+    public ResponseEntity<FruitResponseDTO> createFruit(@Valid @RequestBody FruitRequestDTO request) {
         FruitResponseDTO created = fruitService.addFruit(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<FruitResponseDTO> getById(@PathVariable Long id) {
+    public ResponseEntity<FruitResponseDTO> getFruitById(@PathVariable Long id) {
         return ResponseEntity.ok(fruitService.getFruitById(id));
     }
 
     @GetMapping
-    public ResponseEntity<List<FruitResponseDTO>> list(@RequestParam(required = false) Long supplierId) {
+    public ResponseEntity<List<FruitResponseDTO>> listFruits(@RequestParam(required = false) Long supplierId) {
         if (supplierId != null) {
             return ResponseEntity.ok(fruitService.getFruitsBySupplierId(supplierId));
         }
@@ -41,12 +40,12 @@ public class FruitController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<FruitResponseDTO> update(@PathVariable Long id, @Valid @RequestBody FruitRequestDTO request) {
+    public ResponseEntity<FruitResponseDTO> updateFruit(@PathVariable Long id, @Valid @RequestBody FruitRequestDTO request) {
         return ResponseEntity.ok(fruitService.updateFruit(id, request));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteFruit(@PathVariable Long id) {
         fruitService.deleteFruit(id);
         return ResponseEntity.noContent().build();
     }
